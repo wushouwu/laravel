e6ee1ac245695cf24c111d482df56795716c729a
Your Name <you@example.com>
2018-08-17 21:22:29 +0800

    添加配置

--- resources/assets/components/form/elementInputNumber.vue
+++ resources/assets/components/form/elementInputNumber.vue
@@ -1,10 +1,39 @@
 <template>
     <el-form-item :label="config.label" :prop="config.name">
-        <el-input-number v-model="form[config.name]"  :min="config.min" :max="config.max"></el-input-number>
+        <el-input-number 
+            v-model="form[config.name]"  
+            :min="config.min" 
+            :max="config.max"
+            :size="config.size"
+            :controls-position="config.controlsPosition"
+            :class="{'size-small-font':config.size==='small'?true:false}"
+            @change="change"
+        ></el-input-number>
     </el-form-item>
 </template>
 <script>
     export default {
         props: ['config','form'],
+        methods: {
+            click: function(event){
+                this.$emit('config',event,this.config,{
+                    name:{
+                        type:"elementSelect",
+                        label:"字段",
+                        name:"name",
+                        options:[]
+                    },
+                    label: {
+                        type:"elementText",
+                        label:"字段名",
+                        name:"label"
+                    }
+                });
+            },            
+            change(value){
+                this.$emit('NumberChange',value,this.config);
+                this.$emit('e',value,this.config);
+            }
+        }
     }
 </script>
