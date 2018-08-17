95dbb61885f8283c9aab57b3fed36cf5ec290fba
Your Name <you@example.com>
2018-08-17 21:21:46 +0800

    优化选项配置的方式

--- resources/assets/components/form/elementSelect.vue
+++ resources/assets/components/form/elementSelect.vue
@@ -84,126 +84,130 @@
                         name:"label"
                     },
                     source:{
-                        type:"elementRadio",
+                        type:"elementRadioChange",
                         name:"source",
                         label:"选项来源",
-                        radioChange:`
-                            if(value=="options"){
-                                this.$set(this.attrs.options,'hide',false);
-                                this.$set(this.attrs.tableField,'hide',true);
-                                this.$set(this.attrs.options,'form',this.attrForm.options);
-                            }else{
-                                this.$set(this.attrs.options,'hide',true);
-                                this.$set(this.attrs.tableField,'hide',false);                           
+                        script:`
+                            if(event=="options"){
+                                this.$set(this.attrForm,'options',this.attrForm.options.slice(0,this.attrs.source.options[0].config.options.length));
+                            }else{   
+                                let tableField=this.attrForm.tableField;
+                                if(tableField.table && tableField.fieldLabel && tableField.fieldValue){
+                                    this.my.axios({
+                                        vue: this,
+                                        axiosOption:{
+                                            url: 'admin/table/table',
+                                            data:{
+                                                TABLE_NAME:tableField.table,
+                                                fields:'\`'+tableField.fieldLabel+'\` as label,\`'+tableField.fieldValue+'\` as value',
+                                                pageSize: 10
+                                            }
+                                        },
+                                        success:function(response,option){
+                                            option.vue.$set(option.vue.attrForm,'options',response.data.data);
+                                            
+                                        }
+                                    });
+                                }                                                  
                             }
                         `,
                         options:[{
                             value:'options',
                             label:'自定义',
-                            type:'el-radio'
+                            type:'el-radio',
+                            config:{
+                                type:"elementAddDelete",
+                                name:"options",
+                                labelWidth:'0px',
+                                labels:[{label:'显示值'},{label:'保存值'}],
+                                itemStyle:"width:40%;display:inline-block;margin-right:2%",
+                                options:Array(this.config.options.length).fill([{
+                                    type:"elementText",
+                                    inline:true,
+                                    size:'mini',
+                                    name:"label"
+                                },{
+                                    type:"elementText",
+                                    inline:true,
+                                    size:'mini',
+                                    name:"value"
+                                }])
+                            },
                         },{
                             value:'tableField',
                             label:'表格字段值',
-                            type:'el-radio'
-                        }]
-                    },
-                    tableField:{
-                        hide:this.config.source=='tableField'?false:true,
-                        type:"elementComponents",
-                        name:"options",
-                        labelWidth:"0px",
-                        form:this.config.tableField,
-                        formWatch:`
-                            this.$set(this.form,'tableField',newValue);
-                            if(newValue.table && newValue.fieldLabel && newValue.fieldValue){
-                                this.my.axios({
-                                    vue: this,
-                                    axiosOption:{
-                                        url: 'admin/table/table',
-                                        data:{
-                                            TABLE_NAME:newValue.table,
-                                            fields:'\`'+newValue.fieldLabel+'\` as label,\`'+newValue.fieldValue+'\` as value',
-                                            pageSize: 10
-                                        }
-                                    },
-                                    success:function(response,option){
-                                        option.vue.$set(option.vue.form,'options',response.data.data);
-                                        
+                            type:'el-radio',
+                            config:{
+                                type:"elementComponents",
+                                name:"tableField",
+                                labelWidth:"0px",
+                                formWatch:`
+                                    if(newValue.table && newValue.fieldLabel && newValue.fieldValue){
+                                        this.my.axios({
+                                            vue: this,
+                                            axiosOption:{
+                                                url: 'admin/table/table',
+                                                data:{
+                                                    TABLE_NAME:newValue.table,
+                                                    fields:'\`'+newValue.fieldLabel+'\` as label,\`'+newValue.fieldValue+'\` as value',
+                                                    pageSize: 10
+                                                }
+                                            },
+                                            success:function(response,option){
+                                                option.vue.$set(option.vue.form,'options',response.data.data);
+                                                
+                                            }
+                                        });
                                     }
-                                });
+                                `,
+                                options:[{
+                                    type:"elementSelect",
+                                    name:"table",
+                                    label:"表格",
+                                    size:'small',
+                                    options:this.tables,
+                                    script:`
+                                        this.form.tableField.fieldLabel='';
+                                        this.form.tableField.fieldValue='';                                
+                                        this.my.axios({
+                                            vue: this,
+                                            axiosOption:{
+                                                url: 'admin/table/field',
+                                                data:{
+                                                    TABLE_NAME:event
+                                                }
+                                            },
+                                            success:function(response,option){
+                                                option.vue.config.options[1].options=response.data.data;
+                                                option.vue.config.options[2].options=response.data.data;
+                                                if(response.data.data.length){
+                                                    option.vue.form.tableField.fieldLabel=response.data.data[0].value;
+                                                    option.vue.form.tableField.fieldValue=response.data.data[0].value;                                            
+                                                }
+                                                
+                                            }
+                                        });
+                                    `
+                                },{
+                                    type:"elementSelect",
+                                    name:"fieldLabel",
+                                    label:"显示值",
+                                    size:'small',
+                                    options:this.fields,
+                                },{
+                                    type:"elementSelect",
+                                    name:"fieldValue",
+                                    label:"保存值",
+                                    size:'small',
+                                    options:this.fields,
+                                }]
                             }
-                        `,
-                        options:[{
-                            type:"elementSelect",
-                            name:"table",
-                            label:"表格",
-                            size:'small',
-                            options:this.tables,
-                            event:`
-                                this.config.form.fieldLabel='';
-                                this.config.form.fieldValue='';
-                                this.form.tableField.fieldLabel='';
-                                this.form.tableField.fieldValue='';                                
-                                this.my.axios({
-                                    vue: this,
-                                    axiosOption:{
-                                        url: 'admin/table/field',
-                                        data:{
-                                            TABLE_NAME:event
-                                        }
-                                    },
-                                    success:function(response,option){
-                                        option.vue.config.options[1].options=response.data.data;
-                                        option.vue.config.options[2].options=response.data.data;
-                                        if(response.data.data.length){
-                                            option.vue.config.form.fieldLabel=response.data.data[0].value;
-                                            option.vue.config.form.fieldValue=response.data.data[0].value;
-                                            option.vue.form.tableField.fieldLabel=response.data.data[0].value;
-                                            option.vue.form.tableField.fieldValue=response.data.data[0].value;                                            
-                                        }
-                                        
-                                    }
-                                });
-                            `
-                        },{
-                            type:"elementSelect",
-                            name:"fieldLabel",
-                            label:"显示值",
-                            size:'small',
-                            options:this.fields,
-                        },{
-                            type:"elementSelect",
-                            name:"fieldValue",
-                            label:"保存值",
-                            size:'small',
-                            options:this.fields,
                         }]
-                    },
-                    options:{
-                        type:"elementAddDelete",
-                        hide:this.config.source=='options'?false:true,
-                        name:"options",
-                        labelWidth:'0px',
-                        labels:[{label:'显示值'},{label:'保存值'}],
-                        itemStyle:"width:40%;display:inline-block;margin-right:2%",
-                        options:Array(this.config.options.length).fill([{
-                            type:"elementText",
-                            inline:true,
-                            size:'mini',
-                            name:"label"
-                        },{
-                            type:"elementText",
-                            inline:true,
-                            size:'mini',
-                            name:"value"
-                        }]),
-                        form:this.config.options
                     }
                 });
             },
             change(value){
                 this.$emit('selectChange',value,this.config);
-                //所有组件统一事件
                 this.$emit('e',value,this.config);
             }
         }   
