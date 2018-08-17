e1a97c22bb7406485e8bafde43f4ec87badae12d
Your Name <you@example.com>
2018-08-17 21:20:48 +0800

    添加配置

--- resources/assets/components/form/elementTextarea.vue
+++ resources/assets/components/form/elementTextarea.vue
@@ -1,5 +1,8 @@
 <template>
-    <el-form-item :label="config.label">
+    <el-form-item 
+        :label="config.label"
+        @click.native="click"
+    >
         <el-input 
             type="textarea" 
             v-model="form[config.name]" 
@@ -13,6 +16,55 @@
 </template>
 <script>
     export default {
-        props: ['config','form'],
+        props: ['config','form'],   
+        methods:{
+            click: function(event){
+                this.$emit('config',event,this.config,{
+                    name:{
+                        type:"elementSelect",
+                        label:"字段",
+                        name:"name",
+                        options:[]
+                    },
+                    label: {
+                        type:"elementText",
+                        label:"字段名",
+                        name:"label"
+                    },
+                    readonly: {
+                        type:"elementSwitch",
+                        label:"只读",
+                        name:"readonly"
+                    },
+                    placeholder: {
+                        type:"elementTextarea",
+                        label:"空白提示",
+                        name:"placeholder"
+                    },
+                    autosize:{
+                        type:"elementComponents",
+                        label:"自动尺寸",
+                        name:"autosize",
+                        form:this.config.autosize,                     
+                        options:[{
+                            name:"minRows",
+                            label:"最小行数",
+                            type:"elementInputNumber",
+                            size:"mini",
+                            script:"console.log(this.form,this.config)"
+
+                        },{
+                            name:"maxRows",
+                            label:"最大行数",
+                            type:"elementInputNumber",
+                            size:"mini"
+                        }]
+                    }                                 
+                });
+            },
+            enter(event){
+                this.$emit('enter',event,this.config);
+            }
+        }
     }
 </script>
