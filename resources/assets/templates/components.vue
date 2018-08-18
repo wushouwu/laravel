d2e8db0f1a396b37b540ca0bb550607b52d6fa81
Your Name <you@example.com>
2018-08-18 23:50:06 +0800

    处理组件配置影响源组件的问题

--- resources/assets/templates/components.vue
+++ resources/assets/templates/components.vue
@@ -1,10 +1,32 @@
 <template>
     <el-collapse v-model="activeAccordion" accordion>
+        <el-collapse-item title="表单" name="表单">
+            <el-form :model="form" label-width="80px" style="width:100%;height:100%">
+                <component 
+                    v-for="(config, key,index) in configs.form.fields" 
+                    :key="index" 
+                    :is="config.type" 
+                    :config="config" 
+                    :form="query.row"
+                ></component> 
+                <elementButton
+                    :config="{name:'save',text:'保存',buttonType:'primary'}"
+                    style="text-align:center;"
+                    @buttonClick="save"
+                ></elementButton>            
+            </el-form>
+        </el-collapse-item>         
         <el-collapse-item title="组件" name="组件">
             <el-form :model="form" label-width="80px" style="width:100%;height:100%">
-                <draggable v-model="configs.form" :options="{group:{ name:'view',  pull:'clone', put: false },preventOnFilter: true,handle: 'label',animation: 250}" style="width:100%;height:100%">
+                <draggable 
+                    v-model="configs.components" 
+                    :options="{group:{ name:'view',  pull:'clone', put: false },preventOnFilter: true,handle: 'label',animation: 250}" 
+                    style="width:100%;height:100%"
+                    @remove="remove"
+                    @update="update"
+                >
                     <component 
-                        v-for="(config, key,index) in configs.form" 
+                        v-for="(config, key,index) in configs.components" 
                         :key="index" 
                         :is="config.type" 
                         :config="config" 
@@ -21,8 +43,6 @@
                     :is="attr.type" 
                     :config="attr" 
                     :form="attrForm"
-                    v-show="!attr.hide"
-                    @cascaderActiveChange="cascaderActiveChange"
                     @e="e"
                 ></component>
             </el-form>
@@ -35,84 +55,124 @@
         components: {
             draggable
         },
-        props:['attrs','attrForm'],
+        props:['attrs','attrForm','query'],
         data() {
             return {
-                activeAccordion:'组件',
-                configs: {
-                    form: [{
-                        "type": "elementText",
-                        "label": "文本",
-                        "name": "text"
-                    },{
-                        "type": "elementSelect",
-                        "label": "选项",
-                        "name": "select",
-                        "source":"options",
-                        "options": [{
-                            "value": "1",
-                            "label": "选项1"
-                        }, {
-                            "value": "2",
-                            "label": "选项2"
-                        }],
-                        "tableField":{
-                            "table":'',
-                            "fieldLabel":'',
-                            "fieldValue":''
-                        }
-                    },{
-                        "type": "elementSwitch",
-                        "label": "开关",
-                        "name": "switch"
-                    },{
-                        "type": "elementTextarea",
-                        "label": "多行文本",
-                        "name": "textarea"
-                    },{
-                        "type": "elementDatetime",
-                        "label": "日期时间",
-                        "name": "datetime"
-                    },{
-                        "type": "elementInputNumber",
-                        "label": "数字",
-                        "name": "inputNumber",
-                        "min": 0,
-                        "max": 10
+                activeAccordion:"组件",
+                initComponents:`[{
+                    "type": "elementText",
+                    "label": "文本",
+                    "name": "text"
+                },{
+                    "type": "elementSelect",
+                    "label": "选项",
+                    "name": "select",
+                    "source":"options",
+                    "options": [{
+                        "value": "1",
+                        "label": "选项1"
+                    }, {
+                        "value": "2",
+                        "label": "选项2"
                     }],
-                    table:[{
-                        "type": "elementTable",
-                        "label": "文本",
-                        "name": "text",
-                        'fields':[{
-                            "label": "日期",
-                            "name": "date",
-                            "width": 90,
-                        },{
-                            "label": "姓名",
-                            "name": "name",
-                            "width": 90
-                        },{
-                            "label": "地址",
-                            "name": "province",
-                        }],  
-                    },
-                    ]
+                    "tableField":{
+                        "table":"",
+                        "fieldLabel":"",
+                        "fieldValue":""
+                    }
+                },{
+                    "type": "elementSwitch",
+                    "label": "开关",
+                    "name": "switch"
+                },{
+                    "type": "elementTextarea",
+                    "label": "多行文本",
+                    "name": "textarea",
+                    "autosize":{"minRows":3,"maxRows":5}
+                },{
+                    "type": "elementDatetime",
+                    "label": "日期时间",
+                    "name": "datetime"
+                },{
+                    "type": "elementInputNumber",
+                    "label": "数字",
+                    "name": "inputNumber",
+                    "min": 0,
+                    "max": 10
+                },{
+                    "type": "elementButton",
+                    "labelWidth":"80px",
+                    "label": "按钮",
+                    "text": "按钮",
+                    "shape":"plain",
+                    "buttonType":"primary",
+                    "name": "button"
+                }]`,
+                configs: {
+                    components:[],
+                    form:[]
                 },
                 "form": {
                 }
             }
         },
-        methods:{
-            cascaderActiveChange(value,config){
-                eval(config.cascaderActiveChange);
+        created(){
+            this.configs.components=JSON.parse(this.initComponents);
+            //表单配置
+            this.my.axios({
+                vue: this,
+                axiosOption:{
+                    url:'admin/table/view',
+                    data:{
+                        TABLE_NAME:'view',
+                        view_name:'视图-编辑',
+                        type:'form'
+                    }
+                },
+                success:function(response,option){
+                    option.vue.$set(option.vue.configs,'form',response.data.data);
+                }
+            });            
+        },
+        watch:{
+            json:{
+                handler(newValue, oldValue){
+                    this.$set(this.query.row,'json',JSON.stringify(newValue));
+                },
+                deep:true
+            }
+        },
+        computed:{
+            json(){
+                return this.$parent.$parent.$parent.configs;
+            }
+        },
+        methods:{  
+            //处理源组件被修改的问题
+            remove(event){
+                this.configs.components=JSON.parse(this.initComponents);
+            }, 
+            update(event){
+                this.initComponents=JSON.stringify(this.configs.components);
             },
             e(event,config){
                 switch(config.name){
                     default:
                         eval(config.script);
                 }
-            }
+            },
+            //保存配置
+            save: function(event){
+                this.$set(this.query.row,'json',JSON.stringify(this.json));
+                this.my.axios({
+                    vue: this,
+                    axiosOption:{
+                        url:'/admin/table/save',
+                        data: {form:this.query.row,TABLE_NAME:this.query.TABLE_NAME}
+                    },
+                    successMsg: '保存成功!'
+                });               
+            }           
         }
     }
 </script>
