6ee798a0577710361e0e5e7ee74f4f7e9624ee00
Your Name <you@example.com>
2018-08-31 01:24:40 +0800

    增加key-value的配置

--- resources/assets/components/combine/elementAddDelete.vue
+++ resources/assets/components/combine/elementAddDelete.vue
@@ -7,7 +7,7 @@
         style="line-height:30px;"
         class="elementAddDelete"
     >
-        <div style="line-height:30px;"><label 
+        <div style="line-height:30px;font-size:0px"><label 
                 v-for="(label,key) in config.labels" 
                 :key="key"
                 :style="(config.itemDefault&&config.itemDefault.style?config.itemDefault.style:'')+';text-align:center;font-size:13px;color: #606266;'"
@@ -17,21 +17,62 @@
                 style="width:auto;"
                 @buttonClick="add"
             ></elementButton>            
-        </div>      
-        <div
+        </div> 
+        <!-- options 格式[[{},{}]] --> 
+        <div style="font-size:0px"
+            v-if="0 in config.options && 0 in config.options[0]"
             v-for="(option,key) in config.options"
             :key="key"
         >
             <component 
-                v-for="(item,k) in (option.length?option:config.options)" 
+                v-for="(item,k) in option" 
                 :key="k"
-                v-if="!option.length?key==k:true"
                 :config="Object.assign({},config.itemDefault,item)"
                 :is="item.type"
                 :form="form[config.name][key]"
                 :index="key"
                 @config="itemConfig"
             ></component>
+            <elementButton
+                :config="Object.assign({},config.itemDefault,deleteButton)"
+                style="width:auto;"
+                @buttonClick="del($event,deleteButton,key)"
+            ></elementButton>
+        </div>
+        <!-- options 格式[{},{}] -->
+        <div style="font-size:0px"
+            v-if="0 in config.options && !(0 in config.options[0])"
+            v-for="(option,key) in config.options"
+            :key="key"
+        >
+            <component 
+                :config="Object.assign({},config.itemDefault,option)"
+                :is="option.type"
+                :form="form[config.name][key]"
+                :index="key"
+                @config="itemConfig"
+            ></component>
+            <elementButton
+                :config="Object.assign({},config.itemDefault,deleteButton)"
+                style="width:auto;"
+                @buttonClick="del($event,deleteButton,key)"
+            ></elementButton>
+        </div> 
+        <!-- options 格式{} -->  
+        <div style="font-size:0px"
+            v-if="!config.options.length"
+            v-for="(option,key,index) in config.options"
+            :key="key"
+        >
+            <elementSelect 
+                :config="configHandle(optionKey,String(index))"
+                :form="formKey"
+                @selectChange="selectChange($event,key,index)"
+            ></elementSelect>
+            <elementText 
+                :config="configHandle(optionValue,key)"
+                :form="form[config.name]"
+            ></elementText>            
             <elementButton
                 :config="Object.assign({},config.itemDefault,deleteButton)"
                 style="width:auto;"
@@ -65,8 +106,28 @@
                     plain:true,
                     circle:true,
                     noText:true
+                },
+                formKey:[],
+                optionKey:{
+                    type:'elementSelect',
+                    name:'',
+                    allowCreate:true,
+                    options:[{
+                        label:'url',
+                        value:'url'
+                    },{
+                        label:'表名',
+                        value:'TABLE_NAME'
+                    }]
+                },
+                optionValue:{
+                    type:'elementText',
+                    name:''
                 }
             }
+        },
+        created(){
+            this.formKey=Object.keys(this.form[this.config.name]);
         },
         mounted(){
             this.labelPositionTop();
@@ -82,7 +143,7 @@
                     }
                 },
                 deep:true
-            },
+            }          
         },
         computed:{
             itemForm(){
@@ -101,27 +162,45 @@
                 }
             },            
             add(event,config){
-                this.config.options.push(Object.assign([],this.config.options[0]));
-                let form=Object.assign({},this.form[this.config.name][0]);
-                if('label' in form && 'value' in form){
-                    form.value='';
-                    form.label='';
+                if(this.config.options.length){
+                    this.config.options.push(Object.assign([],this.config.options[0]));
+                    let form=Object.assign({},this.form[this.config.name][0]);
+                    if('label' in form && 'value' in form){
+                        form.value='';
+                        form.label='';
+                    }
+                    this.form[this.config.name].push(form);
+                }else{
+                    let index=String(Object.values(this.config.options).length);
+                    let key='参数'+index;
+                    let option=[this.configHandle(this.optionKey,index),this.configHandle(this.optionValue,key)];
+                    this.$set(this.config.options,key,option);
+                    this.$set(this.form[this.config.name],key,'');
+                    this.formKey=Object.keys(this.form[this.config.name]);
                 }
-                this.form[this.config.name].push(form);
             },
             del(event,config,index){
                 if(this.config.itemDefault.delScript){
                     eval(this.config.itemDefault.delScript);
                 }                
-                if(this.config.options.length>1){
+                if(this.config.options.length>1||this.config.options.length===undefined){
                     this.$delete(this.config.options,index);
                     this.$delete(this.form[this.config.name],index);
+                    this.$delete(this.formKey,this.formKey.findIndex((item)=>item===index));
                 }else{
                     this.$message({
                         message: '最少须有一项',
                         type: 'warning'
                     })
                 }
+            },
+            //处理config
+            configHandle(config,name){
+                return Object.assign({},config,{name:name},this.config.itemDefault);
+            },
+            selectChange(event,key,index){
+                this.$set(this.form[this.config.name],event,this.form[this.config.name][key]);
+                this.$delete(this.form[this.config.name],key);
             },
             click: function(event){
                 this.$emit('config',event,this.config,{
