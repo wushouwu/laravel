143b8f3eeafbc61c5578aa78c8c09070a23cd294
Your Name <you@example.com>
2018-10-02 00:21:21 +0800

    增加并发请求

--- resources/assets/js/my.js
+++ resources/assets/js/my.js
@@ -43,6 +43,41 @@ export var my={
                 type: 'error',
                 duration: 0
             });
+            if(error.message.indexOf(401)>=0 || error.message.indexOf(419)>=0){
+                location.href='login';
+            }
         });
-    }
+    },
+    //请求数据
+    axiosAll: option=>{
+        option = Object.assign(option,{ 
+            errorMsg: '请求失败!'
+        });  
+        let load=option.vue.$loading({
+            target:option.vue.$el,
+            background: 'rgba(0,0,0,0)'
+
+        });
+        let req=[];
+        for(var i in option.axiosOption){
+            req.push(axios(Object.assign(option.axiosOption[i],{
+                method: 'post'
+            })));
+        }
+        axios.all(req).then(axios.spread(function(...res){
+            load.close();
+            option.success(res,option);
+        })).catch((error)=>{
+            load.close();
+            option.vue.$message({
+                showClose: true,
+                message: option.errorMsg+error,
+                type: 'error',
+                duration: 0
+            });
+            if(error.message.indexOf(401)>=0 || error.message.indexOf(419)>=0){
+                location.href='login';
+            }
+        });
+    }    
 }
